03b6d07188d16c83386814fe5237643345eb717d
IsaiahKipyego <kipyegoisaiah@gmail.com>
2023-08-28 10:00:44 -0700

    calculation

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+
+    for j in range(1, 3):
+        try:
+            if (j > a):
+                raise Exception("too far")
+            else:
+                result += (a ** b) / j
+        except Exception:
+            result = b + a
+            break
+    return (result)
