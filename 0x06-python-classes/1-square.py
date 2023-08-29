626eb353c31771b33989b357d64b599bc1ebd302
IsaiahKipyego <kipyegoisaiah@gmail.com>
2023-08-29 17:06:32 +0000

    module that defines a class Square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A module that defines a class Square
+"""
+
+
+class Square:
+    """
+    A class Square defines a private instance attribute size
+    """
+    def __init__(self, size):
+        """
+        initializes size attribute
+        """
+        self.__size = size
