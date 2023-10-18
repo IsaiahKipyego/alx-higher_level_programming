6897946664d73b6730500567084efc27006e7a6e
IsaiahKipyego <kipyegoisaiah@gmail.com>
2023-10-18 13:43:56 +0000

    SQL - More queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
 --lists all privileges of he MySQL users
--- lists privileges of user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- lists privileges of user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
