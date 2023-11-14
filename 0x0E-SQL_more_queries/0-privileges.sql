c9728830ff3ac26211008440467a9a5bc962651e
IsaiahKipyegols <kipyegoisaiah@gmail.com>
2023-11-14 04:31:24 +0000

    JavaScript - Warm up

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
---lists all privileges of he MySQL users
+-- a script that lists privileges of MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
